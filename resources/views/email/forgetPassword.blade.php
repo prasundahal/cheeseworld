c04462807b983b670132372de896705a9936e3cb
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2021-12-08 09:42:42 +0545

    forget password

--- resources/views/email/forgetPassword.blade.php
+++ resources/views/email/forgetPassword.blade.php
@@ -1,4 +1,5 @@
-<h1>Forget Password Email</h1>
+<h1>Reset Password</h1>
    
-You can reset password from bellow link:
-<a href="{{ route('customer.getPasswordResetForm', ['email' => $email, 'token' => $token]) }}">Reset Password</a>
+Click below to reset your password
+<a class="btn btn-primary" href="{{ route('customer.getPasswordResetForm', ['email' => $email, 'token' => $token]) }}">Reset Password</a>
+{{-- <a class="btn btn-primary " style="padding: 10px 15px; background:rgb(67, 8, 230); text-decoration:none; display:block;" href="{{ route('customer.getPasswordResetForm', ['email' => $email, 'token' => $token]) }}">Reset Password</a> --}}
