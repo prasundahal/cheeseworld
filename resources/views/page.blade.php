9222a3b5e2f0706b6b645998fcd42d39c13eba2c
Bipin Joshi <joshibipin2052@gmail.com>
2022-02-01 22:11:02 +0545

    custom page design change

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -6,10 +6,33 @@
 
 <br />
 <div class="container">
-    <h2>{{ $page->page_detail[0]->title }}</h2>
-
-    {!! $page->page_detail[0]->description !!}
+    <section id="breadcrumbs">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <ul class="m-0 p-0 d-flex align-items-center">
+                        <li class="font-weight-bold">Home
+                            </li><span class="mx-2"><i class="fa fa-angle-right"
+                                aria-hidden="true"></i></span>
+                        <li class="">{{ $page->page_detail[0]->title }}</li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    </section>
 </div>
+<div class="page-title" style="background-image: url({{ asset('frontend/image/welcome/product_listing_banner.jpg') }});">
+    <div class="container">
+        <h2>{{ $page->page_detail[0]->title }}</h2>
+    </div>
+</div>
+<div class="" style="margin-top:10px">
+    <div class="container">
+        {!! $page->page_detail[0]->description !!}
+    </div>
+</div>
+
 @else
 <h2>comming soon</h2>
 @endif
