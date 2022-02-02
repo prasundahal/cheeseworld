872e874fad21dfb42319dffef77600614028998f
Bipin Joshi <joshibipin2052@gmail.com>
2022-02-02 23:02:32 +0545

    product link in image

--- resources/views/includes/cart/product_card_style1.blade.php
+++ resources/views/includes/cart/product_card_style1.blade.php
@@ -2,7 +2,9 @@
     <div class="col-xl-3 col-lg-3 col-md-6 col-12 mb-4">
         <div class="content d-flex flex-column bg-white pb-3">
             <div class="image mb-3">
-                <img src="" class="img-fluid product-card-image" alt="">
+                <a class="product-card-link" href="">
+                    <img src="" class="img-fluid product-card-image" alt="">
+                </a>
             </div>
             <div class="heading m-auto">
                 {{-- <a href="javascript:void(0)"> --}}
