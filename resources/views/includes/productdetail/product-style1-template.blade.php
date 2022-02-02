6389e9455c4f484e2dd4747ccc205e90bcb6bf40
Bipin Joshi <joshibipin2052@gmail.com>
2022-02-02 19:25:04 +0545

    product detail slick

--- resources/views/includes/productdetail/product-style1-template.blade.php
+++ resources/views/includes/productdetail/product-style1-template.blade.php
@@ -48,97 +48,110 @@ $newgalleries = array_chunk($galleries, 4, true);
             $(".currentSlider").find(".")
         }
     </script>
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-8 col-lg-8 col-12">
-                <div class="row carousel-gallery-wrapper">
-                    <div class="col-xl-3 col-lg-3 col-12 _boxzoom d-flex align-items-center justify-content-between order-lg-1 order-md-2 order-2">
-                        <!-- Carousel Navigatiom -->
-                        <div class="zoom-thumb">
-                            <div class="product-detail-slider d-flex position-relative">
-                                <?php
-                                $i = 0;
-                                $id = [];
-                                // $j = 1;
-                                ?>
-                                @foreach ($newgalleries as $key => $newgallery)
-                                    @foreach ($newgallery as $v => $gal)
-                                        <div class="slider_item position-relative selected" data-target="#carousel"
-                                            data-slide-to="{{ $v }}">
-                                            <img src="{{ asset('gallary/large' . $gal['name']) }}" class="img-fluid">
+    <section id="product-detail-slider-wrapper" class="py-4">
+        <div class="container">
+            <div class="row py-4">
+                <div class="col-xl-8 col-lg-8 col-12">
+                    <div class="row carousel-gallery-wrapper">
+                        <div class="col-xl-3 col-lg-3 col-12 _boxzoom d-flex align-items-center justify-content-between order-lg-1 order-md-2 order-2">
+                            <!-- Carousel Navigatiom -->
+                            <div class="zoom-thumb">
+                                <div class="product-detail-slider d-flex position-relative">
+                                    <?php
+                                    $i = 0;
+                                    $id = [];
+                                    // $j = 1;
+                                    ?>
+                                    @foreach ($newgalleries as $key => $newgallery)
+                                        @foreach ($newgallery as $v => $gal)
+                                            <div class="slider_item position-relative" data-target="#carousel"
+                                                data-slide-to="{{ $v }}">
+                                                <img src="{{ asset('gallary/large' . $gal['name']) }}" class="img-fluid">
+                                            </div>
+                                        @endforeach
+                                    @endforeach
+                                </div>
+                                {{-- <a class="carousel-control-prev" href="#carousel-thumbs" role="button" data-slide="prev">
+                                    <span class="icon-prv">
+                                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                                    </span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carousel-thumbs" role="button" data-slide="next">
+                                    <span class="icon-nxt">
+                                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                                    </span>
+                                    <span class="sr-only">Next</span>
+                                </a> --}}
+                            </div>
+                        </div>
+                        <div class="col-xl-9 col-lg-9 col-12 order-lg-2 order-md-1 order-1">
+                            <!-- Carousel -->
+                            <div id="carousel" class="carousel slide gallery">
+                                <div class="carousel-inner">
+                                    @foreach ($galleries as $v => $gallery)
+                                        <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                                            <img src="{{ asset('gallary/large' . $gallery['name']) }}" class="img-fluid w-100" />
                                         </div>
                                     @endforeach
-                                @endforeach
+                                </div>
+    
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-9 col-lg-9 col-12 order-lg-2 order-md-1 order-1">
-                        <!-- Carousel -->
-                        <div id="carousel" class="carousel slide gallery">
-                            <div class="carousel-inner">
-                                @foreach ($galleries as $v => $gallery)
-                                    <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
-                                        <img src="{{ asset('gallary/large' . $gallery['name']) }}" class="img-fluid w-100" />
-                                    </div>
-                                @endforeach
-                            </div>
-
+                </div>
+                <div class="col-xl-4 col-lg-4 col-12">
+                    <input type="hidden" id="product_combination_id" />
+                    <div class="d-flex flex-column">
+                        <h1 class="font-weight-bold pro-title mt-3" style="font-size: 25px">
+    
+                        </h1>
+                        <h3 class="font-weight-bold">
+                            {{-- <span class="product-card-price"></span> --}}
+                            <span class="price product-card-price2"></span>
+                        </h3>
+                        <p class="description">
+    
+                        </p>
+                        {{-- <div class="share ml-auto">
+                            <a><i class=" fa fa-share-alt fa-2x" aria-hidden="true"></i></a>
+                        </div> --}}
+                    </div>
+                    <div class="pro-counter mt-2">
+                        <div class="input-group custom_width item-quantity">
+    
+                            <input type="text" id="quantity-input" name="quantity" class="form-control" value="1">
+    
+                            <span class="input-group-btn">
+                                <button type="button" class="quantity-plus btn" data-type="plus" data-field="">
+                                    <i class="fas fa-plus"></i>
+                                </button>
+    
+                                <button type="button" class="quantity-minus btn" data-type="minus" data-field="">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                            </span>
                         </div>
+    
+                     
+                        <button type="button" class="btn  wishlist-icon btn-secondary swipe-to-top mt-2">Add to Wishlist</button>
+                        <button type="button" class="btn btn-secondary swipe-to-top add-to-cart mt-2">Add to Cart</button>
+    
+    
+    
+                    </div>
+                    <div class="pro-options mt-4">
+    
                     </div>
                 </div>
+    
             </div>
-            <div class="col-xl-4 col-lg-4 col-12">
-                <input type="hidden" id="product_combination_id" />
-                <div class="d-flex flex-column">
-                    <h1 class="font-weight-bold pro-title mt-3" style="font-size: 25px">
-
-                    </h1>
-                    <h3 class="font-weight-bold">
-                        {{-- <span class="product-card-price"></span> --}}
-                        <span class="price product-card-price2"></span>
-                    </h3>
-                    <p class="description">
-
-                    </p>
-                    {{-- <div class="share ml-auto">
-                        <a><i class=" fa fa-share-alt fa-2x" aria-hidden="true"></i></a>
-                    </div> --}}
-                </div>
-                <div class="pro-counter mt-2">
-                    <div class="input-group custom_width item-quantity">
-
-                        <input type="text" id="quantity-input" name="quantity" class="form-control" value="1">
-
-                        <span class="input-group-btn">
-                            <button type="button" class="quantity-plus btn" data-type="plus" data-field="">
-                                <i class="fas fa-plus"></i>
-                            </button>
-
-                            <button type="button" class="quantity-minus btn" data-type="minus" data-field="">
-                                <i class="fas fa-minus"></i>
-                            </button>
-                        </span>
-                    </div>
-
-                 
-                    <button type="button" class="btn  wishlist-icon btn-secondary swipe-to-top mt-2">Add to Wishlist</button>
-                    <button type="button" class="btn btn-secondary swipe-to-top add-to-cart mt-2">Add to Cart</button>
-
-
-
-                </div>
-                <div class="pro-options mt-4">
-
-                </div>
+            
+            <div class="row py-5">
+    
             </div>
-
         </div>
-        
-        <div class="row py-5">
-
-        </div>
-    </div>
-
+    </section>
 
 </template>
 <script>
--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -334,54 +334,6 @@
                             } else {
                                 not_combination = 1;
                             }
-                            $('.product-detail-slider').slick({
-        dots: false,
-        arrows: true,
-        autoplay: true,
-        vertical: true,
-        verticalSwiping: true,
-        autoplaySpeed: 3000,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [{
-                breakpoint: 1399,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 780,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                    vertical: false,
-                    verticalSwiping: false,
-                    arrows: false,
-                },
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    vertical: false,
-                    verticalSwiping: false,
-                    arrows: false,
-                },
-            },
-            {
-                breakpoint: 496,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    vertical: false,
-                    verticalSwiping: false,
-                    arrows: false,
-                },
-            },
-        ],
-    });
                             if (not_combination == 0) {
                                 product_combination_id = data.data.product_combination[i]
                                     .product_combination_id;
@@ -427,7 +379,56 @@
             });
 
         })
-
+        $(document).ajaxStop(function() {
+            $('.product-detail-slider').slick({
+                dots: false,
+                arrows: true, // arrow dallo dallo ayo false gare
+                autoplay: true,
+                vertical: true,
+                verticalSwiping: true,
+                autoplaySpeed: 3000,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                responsive: [{
+                        breakpoint: 1399,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
+                        },
+                    },
+                    {
+                        breakpoint: 780,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
+                            vertical: false,
+                            verticalSwiping: false,
+                            arrows: false,
+                        },
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                            vertical: false,
+                            verticalSwiping: false,
+                            arrows: false,
+                        },
+                    },
+                    {
+                        breakpoint: 496,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                            vertical: false,
+                            verticalSwiping: false,
+                            arrows: false,
+                        },
+                    },
+                ],
+            });
+        });
         function fetchRelatedProduct() {
             var url = "{{ url('') }}" + '/api/client/products?limit=10&getDetail=1&language_id=' + languageId +
                 '&currency=' + localStorage.getItem("currency");
